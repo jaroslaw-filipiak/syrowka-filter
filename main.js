d5564284671d1c55fef7f0b856092e81e3689e7c
jaroslaw-filipiak <info@j-filipiak.pl>
2022-12-23 22:22:43 +0100

    it works ?

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const boxes = {
 
         !isActive
           ? boxes.filterBoxes(`${item.dataset.label}`)
-          : boxes.UnfilterBoxes(`${item.dataset.label}`, activeLabels, item);
+          : boxes.UnfilterBoxes2(`${item.dataset.label}`);
       });
     });
   },
@@ -129,6 +129,90 @@ const boxes = {
     };
 
     secondFilter(results, selectedLabelsArr);
+  },
+  UnfilterBoxes2(clickedFilter) {
+    /*
+
+
+	1. sprawdzam sobie  wszystkie aktywne labelki
+	1a. usuwam z tej listy kliknięt obiekt
+	1b. wyciągam z nich dataset i wrzucam do tablicy 
+	z currentLabels 
+
+	2.robie sobie drugą tablice , która to zawiera wszystkie aktualnie zaznaczone  itemy i wyciągam z tych itemów dataset. preffers
+
+	3. robie iteracje po drugiej tablicy i sprawdzam czy każdy element posiada w prefers ciąg znaków zaznaczony w labelce , jeżeli posiada to nie robie z nim nic a jeżeli posiada to wrzucam go do finalnej tablicy z elementami do odznaczenia
+
+	4. robie map po elementach do odznaczenia i usuwam klase is active
+
+	*/
+
+    // odznaczam klikniętą labelke
+    this.removeActiveLabel(clickedFilter);
+
+    // sprawdzam które aktywne labelki zostały i tworze tablice z wszytkimi aktywnymi(zaznaczonymi) labelkami
+    const labels = document.querySelectorAll(`.is-label-active`);
+    const labelsArr = [...labels];
+    const activeLabelsArr = [];
+    labelsArr.map((item) => {
+      activeLabelsArr.push(item.dataset.label);
+    });
+
+    // chciałbym sprawdzić wszystkie aktualnie zaznaczone itemy i zobaczyć czy czasami
+    // jakiś z tych elementów nie ma w sobie dataset.preffers które aktualnie istnieje
+    // w zaznaczonej labelce a raczej w tej tablicy
+
+    // coś mi się zdaje, że trzeba dodać spacje aby to wyodrębnić , zrobić 2 tablice
+    // i je porównać
+
+    // mam 2 tablice : z zaznaczonymi labelkami oraz wyciągam tablice z
+    // każego elementu
+
+    // mam dwa stringi z wartościami oddzielone spacjami
+
+    const activeItems = document.querySelectorAll('.is-item-selected');
+    const activeItemsArr = [...activeItems];
+    const matchingItemsArr = [];
+
+    activeItemsArr.map((item, index) => {
+      const prefersStr = item.dataset.prefers.split(',').join(' ').toString();
+      const activeLabelsStr = activeLabelsArr.toString().split(',').join(' ');
+
+      //   zamiast sprawdzać activeLabelsStr , lepiej zrobić z tego tablice i iterować
+      //   po każdym elemencie tablicy ?
+      //   ale każdy element w itercji musi być osobnym stringiem
+
+      //   console.log('main loop' + index);
+      //   console.log('czy element:');
+      //   console.log(prefersStr);
+      //   console.log('posiada w refsie:');
+      //   console.log(activeLabelsStr); // [0][1] ect
+      //   console.log(activeItemsArr);
+
+      for (let index = 0; index < activeItemsArr.length; index++) {
+        if (prefersStr.includes(activeLabelsArr[index])) {
+          matchingItemsArr.push(item);
+        } else {
+        }
+      }
+    }); // <<--- end map
+
+    // console.log(activeItemsArr);
+
+    // zanim do wspólnych elementów dodam klase is item selected , chciałbym wziąść
+    // wszystkie możliwe elementy i usunąc im tę klasy - aby zostały tylko części wspólne
+
+    activeItemsArr.map((item) => {
+      item.classList.remove('is-item-selected');
+    });
+
+    // wspólnie elementy (zostawiam aktywną klase)
+    // console.log(matchingItemsArr);
+    // matchingItemsArr.classList.add('is-item-selected');
+
+    matchingItemsArr.map((item) => {
+      item.classList.add('is-item-selected');
+    });
   },
   setActiveLabel(cat) {
     const button = document.querySelector(`[data-label='${cat}']`);
